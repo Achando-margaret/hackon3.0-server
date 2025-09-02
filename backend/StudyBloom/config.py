cf1a8a56ddb9150809e81a3683605cde793c13f2
Michelle Kinyua <azariahthesecond@gmail.com>
2025-09-02 11:31:44 +0300

    Imported os

--- backend/StudyBloom/config.py
+++ backend/StudyBloom/config.py
@@ -1,4 +1,7 @@
+import os
+
 class Config:
+
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_default_secret_key'
     # PostgreSQL with psycopg v3
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql+psycopg://username:password@localhost/study_bloom'
